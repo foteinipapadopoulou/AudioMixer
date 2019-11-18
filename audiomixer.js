8f893611c201f63a26b84c621fb604422d97b255
fwteinhpap <34911926+fwteinhpap@users.noreply.github.com>
2019-11-18 13:47:13 +0200

    Adding buttons treble,bass

--- audiomixer.js
+++ audiomixer.js
@@ -1,6 +1,8 @@
 
 const volumeButton=document.getElementById("volume");
 const playButton=document.getElementById("play");
+const bassButton=document.getElementById("bass");
+const trebleButton=document.getElementById("treble");
 var canvasCt=document.getElementById("canvas1");
 var canvasCtx=canvasCt.getContext('2d');
 const compressorButton=document.getElementById("compressor");
@@ -9,10 +11,12 @@ const audioCtx = new AudioContext();
 var mybuffer=null;
 var source=null;
 var analyser=null;
+var bass;
+var treble;
 var gainNode;
 var compressor;
 var state="stopped"
-loadSound("https://dl.dropbox.com/s/fmnarh32xhji2s9/music.mp3?dl=0");
+loadSound("https://dl.dropbox.com/s/roxjy4c2f7z5y6f/The%20Police%20-%20Roxanne%20%28Official%20Music%20Video%29.mp3?dl=0");
 source=playSound(mybuffer)
 
 analyser.fftSize=1024;
@@ -23,6 +27,29 @@ var dataArray = new Uint8Array(bufferLength);
 canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 draw();
 
+bassButton.addEventListener('click',function(){
+  if(bassButton.getAttribute('active')==="true"){
+    bass.gain.setValueAtTime(25,audioCtx.currentTime);
+    bassButton.innerHTML='Disable Bass';
+    bassButton.setAttribute('active',"false");
+  }else{
+    bass.gain.setValueAtTime(0,audioCtx.currentTime);
+    bassButton.innerHTML='Enable Bass';
+    bassButton.setAttribute('active',"true");
+  }
+},false);
+
+trebleButton.addEventListener('click',function(){
+  if(trebleButton.getAttribute('active')==="true"){
+    treble.gain.setValueAtTime(25,audioCtx.currentTime);
+    trebleButton.innerHTML='Disable Treble';
+    trebleButton.setAttribute('active',"false");
+  }else{
+    treble.gain.setValueAtTime(0,audioCtx.currentTime);
+    trebleButton.innerHTML='Enable Treble';
+    trebleButton.setAttribute('active',"true");
+  }
+},false);
 
 compressorButton.addEventListener('click',function(){
     if(compressorButton.getAttribute('active')==="true"){
@@ -59,6 +86,7 @@ playButton.addEventListener('click', function() {
 
 volumeButton.addEventListener('input',function(){
         gainNode.gain.value=this.value/100;
+        console.log(this.value/100)
         document.getElementById("volumedisplay").innerHTML = this.value;
 },false)
 
@@ -79,25 +107,41 @@ function loadSound(url){
 function playSound(buffer){
     analyser=audioCtx.createAnalyser();
     compressor= audioCtx.createDynamicsCompressor();
-    compressor.threshold.setValueAtTime(-50, audioCtx.currentTime);
-compressor.knee.setValueAtTime(40, audioCtx.currentTime);
-compressor.ratio.setValueAtTime(12, audioCtx.currentTime);
-compressor.attack.setValueAtTime(0, audioCtx.currentTime);
-compressor.release.setValueAtTime(0.25, audioCtx.currentTime);	
-    analyser.smoothingTimeConstant = 0.6;
-    source=audioCtx.createBufferSource();
-    source.buffer=mybuffer;
+    bass=audioCtx.createBiquadFilter();
+    treble=audioCtx.createBiquadFilter();
     gainNode=audioCtx.createGain();
+    gainNode.gain.value=0.5;
+    source=audioCtx.createBufferSource();
+
+    compressor.threshold.setValueAtTime(-50, audioCtx.currentTime);
+    compressor.knee.setValueAtTime(40, audioCtx.currentTime);
+    compressor.ratio.setValueAtTime(12, audioCtx.currentTime);
+    compressor.attack.setValueAtTime(0, audioCtx.currentTime);
+    compressor.release.setValueAtTime(0.25, audioCtx.currentTime);
+    analyser.smoothingTimeConstant = 0.6
+    source.buffer=mybuffer;
+
     source.connect(analyser);
-    analyser.connect(gainNode);   
+    analyser.connect(bass);
+    bass.connect(treble);
+    treble.connect(gainNode);
     gainNode.connect(audioCtx.destination);
+    // Manipulate the Biquad filter
+    bass.type="lowshelf";
+    bass.frequency.setValueAtTime(400,audioCtx.currentTime);
+    bass.gain.setValueAtTime(0,audioCtx.currentTime);
+    treble.type = "highshelf";
+    treble.frequency.setValueAtTime(2000, audioCtx.currentTime);
+    treble.gain.setValueAtTime(0, audioCtx.currentTime);
+
+
     return source;
 }
 
     function draw(){
         var drawVisual = requestAnimationFrame(draw);
         analyser.getByteTimeDomainData(dataArray);
-        
+
         canvasCtx.fillStyle = 'rgb(255, 255, 255)';
         canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
         canvasCtx.lineWidth = 1;
@@ -106,22 +150,22 @@ compressor.release.setValueAtTime(0.25, audioCtx.currentTime);
         var sliceWidth = WIDTH * 1.0 / bufferLength;
         var x = 0;
         for(var i = 0; i < bufferLength; i++) {
-   
+
             var v = dataArray[i] / 128.0;
             var y = v * HEIGHT/2;
-    
+
             if(i === 0) {
               canvasCtx.moveTo(x, y);
             } else {
               canvasCtx.lineTo(x, y);
             }
-    
+
             x += sliceWidth;
           }
         canvasCtx.lineTo(WIDTH, HEIGHT/2);
         canvasCtx.stroke();
     };
-    
+
 
 
 
